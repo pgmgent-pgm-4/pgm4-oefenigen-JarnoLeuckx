112ee14500d34c7a233250e7cdf8fefa863a69a9
JarnoLeuckx <jarno.leuckx@student.arteveldehs.be>
2024-04-22 10:16:19 +0200

    add light switch

---
+++ week_02/exercises/switch-lights-on-off/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import DimmedLight from './components/Light/DimmedLight';
+
+function App() {
+  return (
+    <div className="App">
+      <DimmedLight />
+
+    </div>
+  );
+}
+
+export default App;
