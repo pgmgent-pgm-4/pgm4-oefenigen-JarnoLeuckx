e56a867da42ef137befa0743bee10eebf3ab38b3
JarnoLeuckx <jarno.leuckx@student.arteveldehs.be>
2024-04-22 09:13:45 +0200

    add first demo

--- week_02/demos/demo/src/App.js
+++ week_02/demos/demo/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter';
+
 
 function App() {
   return (
     <div className="App">
     
-    
+    <Counter/>
     </div>
   );
 }
