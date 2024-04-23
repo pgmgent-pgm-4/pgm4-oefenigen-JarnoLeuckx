fafe6e3c58bade2d83590e740689c551558f7ddb
JarnoLeuckx <jarno.leuckx@student.arteveldehs.be>
2024-04-23 13:52:37 +0200

    commit

---
+++ week_02/exercises/kleuren/components/Colers.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Colers() {
+    const [ListColers, setListColers] = useState([
+        'rgb(10,30,55'
+    ])
+  return (
+    <div>Colers</div>
+  )
+}
