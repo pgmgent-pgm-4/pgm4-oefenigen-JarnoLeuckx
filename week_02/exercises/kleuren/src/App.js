d0adf74041e21a25072ba43f5ccf429088fc7026
JarnoLeuckx <jarno.leuckx@student.arteveldehs.be>
2024-04-23 15:02:01 +0200

    💫 add oefening kleuren

---
+++ week_02/exercises/kleuren/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import Colors from "./components/Colors";
+
+function App() {
+	return (
+		<div className="App">
+			
+			<Colors />
+		</div>
+	);
+}
+
+export default App;
