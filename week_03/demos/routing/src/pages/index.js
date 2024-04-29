16306e5341157d7a1a19668b67c6e65f2d971939
JarnoLeuckx <jarno.leuckx@student.arteveldehs.be>
2024-04-29 13:27:49 +0200

    adc demo routing

---
+++ week_03/demos/routing/src/pages/index.js
@@ -0,0 +1,8 @@
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import NotFound from "./NotFound";
+import Blog from "./Blog";
+import BlogPost from "./BlogPost";
+
+export { About, Home, Contact, Blog, NotFound, BlogPost };
