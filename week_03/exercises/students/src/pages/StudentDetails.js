e16055a04aba61bca64a67f58bfbfb6ce1b434fd
JarnoLeuckx <jarno.leuckx@student.arteveldehs.be>
2024-04-30 15:17:30 +0200

    update student app

--- week_03/exercises/students/src/pages/StudentDetails.js
+++ week_03/exercises/students/src/pages/StudentDetails.js
@@ -1,8 +1,20 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { useState } from "react";
+import allStudents from "../data/student.js";
+import Student from "../components/students/Student.js";
 
 export default function StudentDetails() {
 	const { studentId } = useParams();
+	const [student, setStudent] = useState(null);
+	useEffect(() => {
+		
+			const newStudent= allStudents.filter((student) => student.id === studentId);
+			setStudent(newStudent[0]);
+	}, []);
 
-	return <div>StudentDetails: {studentId}</div>;
+	return <div>StudentDetails: {studentId}
+	{student && <Student student={student} />}
+	
+	</div>;
 }
