f8027ca297934aa28c03cc23b16d1ae50bd7df8f
JarnoLeuckx <jarno.leuckx@student.arteveldehs.be>
2024-05-06 10:36:55 +0200

    opdate demo

--- week_04/demos/context-customhooks/src/App.js
+++ week_04/demos/context-customhooks/src/App.js
@@ -1,6 +1,8 @@
-
+import logo from "./logo.svg";
 import "./App.css";
 import { useFetch } from "./hooks/useFetch";
+import { ThemeArea } from "./context/ThemeContext";
+import ThemeSwitcher from "./components/ThemeSwitcher";
 
 const url = "https://fakestoreapi.com/products";
 function App() {
@@ -9,11 +11,12 @@ function App() {
 	if (isError) return <>...Error</>;
 	console.log(products);
 	return (
-		<>
+		<ThemeArea>
+			<ThemeSwitcher />
 			{products.map((product) => (
 				<div key={product.id}>{product.title}</div>
 			))}
-		</>
+		</ThemeArea>
 	);
 }
 
